daeb08694c20b9cd9e9bebd1242c487cf0cc34f5
Carl Emil Zeidler <carlemilzeidler@gmail.com>
2026-05-05 13:09:43 +0200

    add test for CI

---
+++ src/test/MainTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+class MainTest {
+    @Test
+    void simpleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
