576f099730dd350bbbbbf64490d125dfc2368c02
Carl Emil Zeidler <carlemilzeidler@gmail.com>
2026-05-05 13:18:34 +0200

    add test for CI

--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -1,8 +1,8 @@
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
+
     @Test
     void simpleTest() {
         assertEquals(2, 1 + 1);
