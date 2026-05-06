76f28fd83da9e16db6354056d68c5b36e065734a
Tor <torschoenbech@gmail.com>
2026-05-06 11:44:55 +0200

    Lav Member class #12

---
+++ src/main/java/Member.java
@@ -0,0 +1,9 @@
+public class Member {
+    private String Name;
+    private int BithYear;
+
+    public Member(String name, int BirthYear) {
+        this.Name = name;
+        this.BithYear = BirthYear;
+    }
+}
