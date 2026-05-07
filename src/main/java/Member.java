91e40506b22c8ff6e1d8f8dddad20c54cd3ece3b
Tor <torschoenbech@gmail.com>
2026-05-07 11:08:11 +0200

    Lav Member klasse færdig

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,9 +1,43 @@
-public class Member {
-    private String Name;
-    private int BithYear;
+import java.time.Year;
 
-    public Member(String name, int BirthYear) {
-        this.Name = name;
-        this.BithYear = BirthYear
+public class Member {
+    private String name;
+    private int birthYear;
+    private boolean isActive;
+    private boolean isCompeditor;
+
+    public Member(String name, int birthYear, boolean isActive, boolean isCompeditor) {
+        this.name = name;
+        this.birthYear = birthYear;
+        this.isActive = isActive;
+        this.isCompeditor = isCompeditor;
+    }
+    public int getAge() {
+        int currentYear = Year.now().getValue();
+        return currentYear - birthYear;
+    }
+    public boolean isJunior() {
+        return getAge() <18;
+
+    }
+
+    public boolean isSenior() {
+        return getAge() >= 18;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public boolean isCompeditor() {
+        return isCompeditor;
     }
 }
