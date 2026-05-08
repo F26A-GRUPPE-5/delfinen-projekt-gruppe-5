7dff5dcad5b71fc31d3ef843288204b3deb517fe
Carl Emil Zeidler <carlemilzeidler@gmail.com>
2026-05-08 10:26:15 +0200

    feature/serializer

--- src/main/java/dk/delfinen/gruppe5/adapter/in/Controller.java
+++ src/main/java/dk/delfinen/gruppe5/adapter/in/Controller.java
@@ -1,5 +1,5 @@
 package dk.delfinen.gruppe5.adapter.in;
-
+// Adapter laget kaldes også infrastructure i Clean Architecture
 //TODO: Denne skal nok deles op i 2:
 // 1. Router - Her defineres menu navigationen, hvilken text der skal stå, og der matches med en use case.
 // 2. InputHandler - skal sørges for at hvis input er forkert type, gentages prompten til brugeren. Her kan også parses, altså gøre brugerinput læsbart
