38605a9fe7816adf62b036cb9324d1d17244403d
Tor <torschoenbech@gmail.com>
2026-05-08 12:18:19 +0200

    Lav subscription klasse

--- src/main/java/dk/delfinen/gruppe5/Main.java
+++ src/main/java/dk/delfinen/gruppe5/Main.java
@@ -41,7 +41,7 @@ public class Main {
             System.out.println(m);
 
         }
-        System.out.println("Har ikke betalt");
+        System.out.println("Har ik betalt");
         for (Member m : subscription.getUnpaidMembers()) {
             System.out.println(m);
 
