d63cf2e946f463ed9bab56a925cdcf0ad4b14885
Tor <torschoenbech@gmail.com>
2026-05-15 09:45:06 +0200

    Lav inputParser

--- src/main/java/dk/delfinen/gruppe5/adapter/in/InputParser.java
+++ src/main/java/dk/delfinen/gruppe5/adapter/in/InputParser.java
@@ -19,4 +19,37 @@ public class InputParser {
         scanner.nextLine();
         return returnValue;
     }
+
+    public String parseString() throws InvalidInputException {
+        String input = scanner.nextLine().trim();
+        System.out.println("Input: '" + input + "' længde: " + input.length());
+
+        if (input.isEmpty()) {
+            throw new InvalidInputException("Input må ikke være tomt.");
+        }
+
+        for (char c : input.toCharArray()) {
+            if (!Character.isLetterOrDigit(c) && c != ' ') {
+                throw new InvalidInputException("Input indeholder ugyldige tegn.");
+            }
+        }
+
+        if (input.length() > 25) {
+            throw new InvalidInputException("Input er for langt.");
+        }
+
+        return input;
+    }
+
+    public boolean parseBoolean() throws InvalidInputException {
+        String input = scanner.nextLine().trim();
+
+        if (input.isEmpty() || input.equals("måske") || input.equals("1")) {
+            scanner.nextLine();
+            return true;
+        }
+
+        throw new InvalidInputException("Ugyldigt input.");
+
+    }
 }
